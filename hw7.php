1c7e8ea199379851d18b58d8ec739c842c4a41f2
Pranav Sethi <96.pranav@gmail.com>
2018-04-07 19:27:09 -0400

    fixes

--- hw7.php
+++ hw7.php
@@ -6,26 +6,23 @@ $data = "CSE356";
 
 $table = "assists";
 
-$mem = new Memcached();
-$mem->addServer("127.0.0.1", 11211);
-
-$q = "SELECT a.Player AS player, a.Club AS club, a.POS AS pos, MAX(a.A) AS max_a, avg_assists FROM `assists` AS a, `assists` AS b INNER JOIN (SELECT AVG(c.A) AS avg_assists, c.POS, c.Club FROM `assists` c GROUP BY Club, POS) AS d WHERE a.GS > b.GS AND d.Club = a.Club AND a.Club = b.Club AND a.POS = b.POS AND d.POS = a.POS AND a.POS = ? AND a.Club = ?";
+// $mem = new Memcached();
+// $mem->addServer("127.0.0.1", 11211);
 
 $pos = $_GET['pos'];
-$club = $_GET['club'];
+$club = $_GET['club']; 
+
+$q = "SELECT a.Player AS player, a.Club as club, a.POS as pos, MAX(a.A) as max_assists, avg_assists FROM `assists` AS a, `assists` AS b INNER JOIN (SELECT AVG(c.A) AS avg_assists, c.POS, c.Club FROM `assists` c GROUP BY Club, POS) AS d WHERE a.GS > b.GS AND d.Club = a.Club AND a.Club = b.Club AND a.POS = b.POS AND d.POS = a.POS AND a.POS = '". $pos ."' AND a.Club = '" . $club ."'" ;
 
 $key = $pos.'@'.$club;
 
-$conn = new mysqli($host, $username, $password, $data);
+$conn = mysqli_connect($host, $username, $password, $data);
+$query = mysqli_query($conn, $q);
 
-$stmt = $conn->prepare($q);
-$stmt = $conn->bind_param("ss", $pos, $club);
-$stmt = $conn->execute();
-
-$res = $stmt->get_result();
-
-while ($row = $res->fetch_assoc()){
-	print_r($row);
+while ($row = mysqli_fetch_assoc($query)){
+	echo json_encode($row);
 }
+
+// echo $q;
 
 ?>
